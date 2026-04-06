62a6047fabf5b968f62cd84f00a916bbcdc0c582
helovd <heloisavidaldourado@outlook.com>
2026-04-06 19:02:49 -0300

    Controller adicionado

--- src/main/java/com/ayvy/api_java/controller/ClienteController.java
+++ src/main/java/com/ayvy/api_java/controller/ClienteController.java
@@ -1,4 +1,49 @@
 package com.ayvy.api_java.controller;
 
+import com.ayvy.api_java.business.ClienteService;
+import com.ayvy.api_java.infrastructure.entitys.Cliente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+//A uri:
+//Vai fazer requisições pela 'localhost:8080/cliente'
+@RequestMapping("/cliente")
+
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+    //Para gravar dados:
+    @PostMapping
+    public ResponseEntity<Void> salvarCliente(@RequestBody Cliente cliente) {  //Necessário alterar para DTO
+        clienteService.salvarCliente(cliente);
+        return ResponseEntity.ok().build();
+    }
+
+    //Para buscar:
+    @GetMapping
+    public ResponseEntity<Cliente> buscarClientePorEmail(@RequestParam String email) {
+        return ResponseEntity.ok(clienteService.buscarClientePorEmail(email));
+    }
+
+    //Para deletar:
+    @DeleteMapping
+    public ResponseEntity<Void> deletarClientePorEmail(@RequestParam String email){
+        clienteService.deletarClientePorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+
+    //Fazer alterações em TODOS os campos:
+    @PutMapping
+    public ResponseEntity<Void> atualizarClientePorId(@RequestParam long id,
+                                                      @RequestBody Cliente cliente){
+        clienteService.atualizarClientePorId(id, cliente);
+        return ResponseEntity.ok().build();
+    }
+
 }
+
--- src/main/java/com/ayvy/api_java/controller/ProdutoController.java
+++ src/main/java/com/ayvy/api_java/controller/ProdutoController.java
@@ -1,4 +1,50 @@
 package com.ayvy.api_java.controller;
 
+import com.ayvy.api_java.business.ProdutoService;
+import com.ayvy.api_java.infrastructure.entitys.Produto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+//A uri:
+//Vai fazer requisições pela 'localhost:8080/cliente'
+@RequestMapping("/produto")
+
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+    //Para gravar dados:
+    @PostMapping
+    public ResponseEntity<Void> salvarCliente(@RequestBody Produto produto) {  //Necessário alterar para DTO
+        produtoService.salvarProduto(produto);
+        return ResponseEntity.ok().build();
+    }
+
+    //Para buscar:
+    @GetMapping
+    public ResponseEntity<Produto> buscarProdutoPorId(@RequestParam Long id) {
+        return ResponseEntity.ok(produtoService.buscarProdutoPorId(id));
+    }
+
+    //Para deletar:
+    @DeleteMapping
+    public ResponseEntity<Void> deletarProdutoPorId(@RequestParam Long id){
+        ProdutoService.deletarProdutoPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    //Fazer alterações em TODOS os campos:
+    @PutMapping
+    public ResponseEntity<Void> atualizarProdutoPorId(@RequestParam long id,
+                                                      @RequestBody Produto produto){
+        ProdutoService.atualizarProdutoPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
+
+
