6877a428a15a38089c484595800b9f778f47e9f0
helovd <heloisavidaldourado@outlook.com>
2026-04-15 22:30:30 -0300

    Entity Categoria add

---
+++ src/main/java/com/ayvy/api_java/infrastructure/entitys/Categoria.java
@@ -0,0 +1,24 @@
+package com.ayvy.api_java.infrastructure.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "categoria")
+@Entity
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //permitir apenas números? a organização, como em telefone o padrão '11 9xxxx-xxxx', é com o front?
+    @Column(name = "nome_categoria")
+    private String nome_categoria;
+
+}
